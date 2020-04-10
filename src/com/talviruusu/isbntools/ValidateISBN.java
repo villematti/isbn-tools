a2bf60f099f8f347b1a21d68300b43931a8b4f1d
Ville-Matti Hakanpää <ville-matti.hakanpaa@futuriot.com>
2020-04-10 10:56:53 +0300

    Refactor code on validator.

--- src/com/talviruusu/isbntools/ValidateISBN.java
+++ src/com/talviruusu/isbntools/ValidateISBN.java
@@ -2,25 +2,58 @@ package com.talviruusu.isbntools;
 
 public class ValidateISBN {
 
+    public static final int LONG_ISBN_LENGTH = 13;
+    public static final int SHORT_ISBN_LENGTH = 10;
+    public static final int SHORT_ISBN_MULTIPLIER = 11;
+    public static final int LONG_ISBN_MULTIPLIER = 10;
+
     public boolean checkISBN(String isbn) {
-        if (isbn.length() != 10) throw new NumberFormatException("ISBN number must be 10 digits long.");
 
-        double total = 0.0;
+        if(isbn.length() == LONG_ISBN_LENGTH) {
+            return is13DigitsISBN(isbn);
+        } else {
+            return  is10DigitsISBN(isbn);
+        }
+    }
 
-        for (int i = 0;i < 10;i++) {
+    private boolean is10DigitsISBN(String isbn) {
+        if (isbn.length() != SHORT_ISBN_LENGTH) throw new NumberFormatException("ISBN number must be 10 digits long.");
+
+        int total = 0;
+
+        for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
             char currentChar = isbn.charAt(i);
             if (!Character.isDigit(currentChar)) {
                 if (i == 9 && currentChar == 'X') {
                     total += 10;
                 } else {
-                    throw  new NumberFormatException("All values on ISBN must be numeric digits");
+                    throw new NumberFormatException("All values on ISBN must be numeric digits");
                 }
             } else {
-                total += Character.getNumericValue(currentChar) * (10 - i);
+                total += Character.getNumericValue(currentChar) * (SHORT_ISBN_LENGTH - i);
             }
         }
 
-        if (total % 11 == 0.0) {
+        if (total % SHORT_ISBN_MULTIPLIER == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean is13DigitsISBN(String isbn) {
+        int total = 0;
+
+        for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+            char currentChar = isbn.charAt(i);
+            if (i % 2 == 0) {
+                total += Character.getNumericValue(currentChar);
+            } else {
+                total += Character.getNumericValue(currentChar) * 3;
+            }
+        }
+
+        if (total %  LONG_ISBN_MULTIPLIER == 0) {
             return true;
         } else {
             return false;
--- test/com/talviruusu/isbntools/ValidateISBNTest.java
+++ test/com/talviruusu/isbntools/ValidateISBNTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class ValidateISBNTest {
 
     @Test
-    public void checkAValidISBN() {
+    public void checkAValid10DigitsISBN() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result =  validateISBN.checkISBN("0140449116");
         assertTrue("first value", result);
@@ -16,16 +16,32 @@ public class ValidateISBNTest {
     }
 
     @Test
-    public void ISBNNumbersEndingInAnXAreValid() {
+    public void TenDigitsISBNNumbersEndingInAnXAreValid() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result =  validateISBN.checkISBN("012000030X");
         assertTrue(result);
     }
 
     @Test
-    public void checkAnInvalidISBN() {
+    public void checkAnInvalid10DigitsISBN() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result = validateISBN.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkAValid13DigitsISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("9781644450000");
+        assertTrue("first value", result);
+        result = validateISBN.checkISBN("9781853260087");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void checkAnInvalid13DigitsISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("9781644450006");
         assertFalse(result);
     }
 
