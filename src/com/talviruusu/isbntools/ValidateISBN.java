c1eaf41ba2883e0d4702de9366c30c4c7ec64f15
Ville-Matti Hakanpää <ville-matti.hakanpaa@futuriot.com>
2020-04-10 10:00:24 +0300

    Non numeric ISBN number is not allowed.

--- src/com/talviruusu/isbntools/ValidateISBN.java
+++ src/com/talviruusu/isbntools/ValidateISBN.java
@@ -2,7 +2,21 @@ package com.talviruusu.isbntools;
 
 public class ValidateISBN {
 
-    public boolean checkISBN(int isbn) {
-        return true;
+    public boolean checkISBN(String isbn) {
+        if (isbn.length() != 10) throw new NumberFormatException("ISBN number must be 10 digits long.");
+
+        double total = 0.0;
+
+        for (int i = 0;i < 10;i++) {
+            char currentChar = isbn.charAt(i);
+            if (!Character.isDigit(currentChar)) throw  new NumberFormatException("All values on ISBN must be digits");
+            total += currentChar * (10 - i);
+        }
+
+        if (total % 11 == 0.0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- test/com/talviruusu/isbntools/ValidateISBNTest.java
+++ test/com/talviruusu/isbntools/ValidateISBNTest.java
@@ -2,14 +2,35 @@ package com.talviruusu.isbntools;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ValidateISBNTest {
 
     @Test
-    public void checkValidISBN() {
+    public void checkAValidISBN() {
         ValidateISBN validateISBN = new ValidateISBN();
-        boolean result =  validateISBN.checkISBN(140449116);
-        assertTrue(result);
+        boolean result =  validateISBN.checkISBN("0140449116");
+        assertTrue("first value", result);
+        result = validateISBN.checkISBN("0140177396");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void checkAnInvalidISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test(expected = NumberFormatException.class )
+    public void nineDigitIsNotAllowed() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        validateISBN.checkISBN("123456789");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nonNumericISBNAreNotAllowed() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        validateISBN.checkISBN("helloworld");
     }
 }
