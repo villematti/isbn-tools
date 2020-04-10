15309653a64696594c9bbaf6571e9009d0d169f6
Ville-Matti Hakanpää <ville-matti.hakanpaa@futuriot.com>
2020-04-10 09:09:20 +0300

    Create first test for checking ISBN number.

--- src/com/talviruusu/isbntools/ValidateISBN.java
+++ src/com/talviruusu/isbntools/ValidateISBN.java
@@ -1,4 +1,8 @@
 package com.talviruusu.isbntools;
 
 public class ValidateISBN {
+
+    public boolean checkISBN(int isbn) {
+        return true;
+    }
 }
--- test/com/talviruusu/isbntools/ValidateISBNTest.java
+++ test/com/talviruusu/isbntools/ValidateISBNTest.java
@@ -1,4 +1,15 @@
 package com.talviruusu.isbntools;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class ValidateISBNTest {
+
+    @Test
+    public void checkValidISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result =  validateISBN.checkISBN(140449116);
+        assertTrue(result);
+    }
 }
